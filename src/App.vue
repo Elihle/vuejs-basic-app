97b81c49146f42ffc77ad15f11c81443075fb4f2
lihle <lihlemenz@gmail.com>
2020-02-06 12:52:18 +0200

    added data methods for employees

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app" class="small-container">
     <h1>Employees</h1>
-    
-    <employee-table/>
+
+    <employee-table v-bind:employees="employees"/>
   </div>
 </template>
 
@@ -13,9 +13,39 @@ export default {
   name: 'app',
   components: {
     EmployeeTable,
+  },
+
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: 'Lihle Menzeleleli',
+          email: 'lihle@codex.com'
+        },
+        {
+          id: 2,
+          name: 'Andre Vermuleun',
+          email: 'andre@codex.com',
+        },
+        {
+          id: 3,
+          name: 'Pholisa Fatyela',
+          email: 'pholisa@codex.com'
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
+  button {
+    background: green;
+    border: 1px solid green;
+  }
+
+  .small-container {
+    max-width: 680px;
+  }
 </style>
